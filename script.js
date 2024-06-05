ae1414aeeae63ba521caab02697ff9ba994ca255
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-06-05 18:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 function firstWord(str) {
-  // Trim the input string to remove leading and trailing spaces
-  str = str.trim();
+    let str1 = str.split(" ");    
+    return str1[0];     
+} 
 
-  // Find the index of the first space
-  const firstSpaceIndex = str.indexOf(' ');
-
-  // If there is no space or the string is empty, return the whole string
-  if (firstSpaceIndex === -1) {
-    return '""'str; '""'
-  }
-
-  // Return the substring from the start of the string to the first space
-  return str.substring(0, firstSpaceIndex);
-}
-
-// Examples
-/*console.log(firstWord('see and stop')); // Output: 'see'
-console.log(firstWord(' Hello World!')); // Output: 'Hello'
-console.log(firstWord('12345')); // Output: '12345'
-console.log(firstWord('')); // Output: ''*/
-const s = prompt("Enter String:");
-alert(firstWord(s));
-    
+console.log(firstWord('Hello World'));
 
